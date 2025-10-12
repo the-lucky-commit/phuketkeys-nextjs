ac42021be28840fc84fd7e0935914cd3c79b9864
the-lucky-commit <lucky99.commit@gmail.com>
2025-10-13 01:36:45 +0700

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // --- เพิ่มส่วนนี้เข้ามา ---
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // -----------------------
+};
 
 export default nextConfig;
